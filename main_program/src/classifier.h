0b2d7fe4cbcf1f8b7a4faebef8dd2815268cafa3
mholdg16 <mholdg16@student.aau.dk>
2018-12-13 07:51:59 +0100

    Added calculate threshold and edits

--- main_program/src/classifier.h
+++ main_program/src/classifier.h
@@ -4,16 +4,20 @@
 #define PROB_THRESHOLD 0.003
 
 #include <stdint.h>
+#include <math.h>
 
 #include "headline.h"
 #include "features.h"
+#include "double_array.h"
 
-int classify( Headline *headline, Feature *features, double threshold );
+int8_t classify( Headline *headline, Feature *features, double threshold );
 void classify_array( Headline *headlines, uint16_t headline_count, Feature *features, double threshold );
 Feature* calculate_feature_array( Headline* headlines, uint16_t headline_count );
+double calculate_threshold(Headline* headlines, uint16_t headline_count, Feature *features);
 
 void _set_feature_vector( Headline *headline, Feature *features );
 void _add_feature_count( Headline headline, Feature *features );
-double _calculate_cb_prob( Headline headline, Feature* features );
+double _calculate_cb_prob( Headline headline, Feature *features );
+double _bernoulli_nb( Headline headline, Feature *features );
 
 #endif
