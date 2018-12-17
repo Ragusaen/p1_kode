8cd6b11dfd0e795726485fbbb1186e714c673a53
mholdg16 <mholdg16@student.aau.dk>
2018-12-17 08:06:02 +0100

    Added score_dataset and _score_headline

--- main_program/src/classifier.h
+++ main_program/src/classifier.h
@@ -11,8 +11,10 @@
 
 void classify_dataset(DataSet set, FeatureSet featureset, double threshold);
 int8_t classify(Headline *headline, FeatureSet featureset, double threshold);
+void score_dataset(DataSet set, FeatureSet featureset);
 double calculate_threshold(DataSet set, FeatureSet featureset);
 
+void _score_headline(Headline *headline, FeatureSet featureset);
 uint8_t _get_feature_vector(char str_in[], FeatureSet featureset);
 double _calculate_cb_prob(uint8_t feature_vector, FeatureSet featuresset);
 double _prob_given_not_feature(double pcbf, double pf);
