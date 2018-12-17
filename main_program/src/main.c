284b4121f2c65af58f06052bb0723ed6958e6a58
mholdg16 <mholdg16@student.aau.dk>
2018-12-17 12:14:24 +0100

    Header

--- main_program/src/main.c
+++ main_program/src/main.c
@@ -1,8 +1,10 @@
-/**
- *  Automatic detection of clickbait
- *
- *
- **/
+/******************************************************************************
+ * Automatic detection of clickbait
+ * 
+ * @version     3.2
+ * @studygroup  DAT1A307
+ * @year        2018
+ ******************************************************************************/
 
 #include <stdlib.h>
 
