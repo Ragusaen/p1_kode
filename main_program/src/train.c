c71c7122fb2c0c134b03d88daa2c312bd14d982e
mholdg16 <mholdg16@student.aau.dk>
2018-12-17 08:02:35 +0100

    Added load/save function

--- main_program/src/train.c
+++ main_program/src/train.c
@@ -1,12 +1,38 @@
 #include "train.h"
 
+int load_trained_features(FeatureSet *featureset)
+{
+    int i;
+    FILE *fp;
+    FeatureSet temp;
+
+    *featureset = import_features();
+    if ((temp.features = (Feature*) calloc(featureset->count, sizeof(Feature))) == NULL) fatal_error();
+
+    if ((fp = fopen(TRAINED_FEATURES_FN, "r")) == NULL) {
+        error("Couldn't load \"" TRAINED_FEATURES_FN "\"");
+        return 0;
+    }
+
+    for (i = 0; i < featureset->count; i++) {
+        fread(temp.features + i, sizeof(Feature), 1, fp);
+        _copy_feature(featureset->features + i, temp.features[i]);
+    }
+
+    fclose(fp);
+
+    return 1;
+}
+
+
 /**
  * Trains the features, eg. calculates probabilities.
  * 
  * @param set   the training data set
  */
 
-FeatureSet train_features(DataSet set) {
+FeatureSet train_features(DataSet dataset)
+{
     uint16_t i;
     FeatureSet featureset;
     
@@ -14,19 +40,22 @@ FeatureSet train_features(DataSet set) {
     featureset = import_features();
 
     /* count number of features for each headline */
-    for ( i = 0; i < set.count; i++ ) {
-        _add_feature_count(set.data[i], featureset);
+    for ( i = 0; i < dataset.count; i++ ) {
+        _add_feature_count(dataset.data[i], featureset);
     }
 
     /* calculate probabilities for each feature */
     for ( i = 0; i < featureset.count; i++ ) {
-        _calculate_feature_probabilities(featureset.features + i, set.count);
+        _calculate_feature_probabilities(featureset.features + i, dataset.count);
     }
+
+    _save_trained_features(featureset);
 
     return featureset;
 }
 
-void _add_feature_count(Headline headline, FeatureSet featureset) {
+void _add_feature_count(Headline headline, FeatureSet featureset)
+{
     uint8_t j;
     for ( j = 0; j < featureset.count; j++ ) {
         if (featureset.features[j].has_feature(headline.content)) {
@@ -38,7 +67,8 @@ void _add_feature_count(Headline headline, FeatureSet featureset) {
     }
 }
 
-void _calculate_feature_probabilities(Feature *feature, int data_count) {
+void _calculate_feature_probabilities(Feature *feature, int data_count)
+{
     /* p(F) = |F|/|A| */
     feature->prob_feature = (double) feature->feature_count / data_count;
 
@@ -49,4 +79,32 @@ void _calculate_feature_probabilities(Feature *feature, int data_count) {
     else {
         feature->prob_cb_given_feature = 1;
     }
+}
+
+
+int _save_trained_features(FeatureSet featureset)
+{
+    int i;
+    FILE *fp;
+
+    if ((fp = fopen(TRAINED_FEATURES_FN, "w")) == NULL) {
+        error("Couldn't save \"" TRAINED_FEATURES_FN "\"");
+        return 0;
+    }
+
+    for (i = 0; i < featureset.count; i++) {
+        fwrite(featureset.features + i, sizeof(Feature), 1, fp);
+    }
+
+    fclose(fp);
+
+    return 1;
+}
+
+void _copy_feature(Feature *target, Feature original)
+{
+    target->prob_feature = original.prob_feature;
+    target->prob_cb_given_feature = original.prob_cb_given_feature;
+    target->feature_count = original.feature_count;
+    target->feature_cb_count = original.feature_cb_count;
 }
--- main_program/src/train.h
+++ main_program/src/train.h
@@ -7,9 +7,15 @@
 #include "headline.h"
 #include "features.h"
 
-FeatureSet train_features(DataSet set);
+#define TRAINED_FEATURES_FN "trainedfeatures.bin"
+
+int load_trained_features(FeatureSet *featureset);
+FeatureSet train_features(DataSet dataset);
 
 void _add_feature_count(Headline headline, FeatureSet featureset);
 void _calculate_feature_probabilities(Feature *feature, int data_count);
+
+int _save_trained_features(FeatureSet featureset);
+void _copy_feature(Feature *target, Feature original);
 
 #endif
